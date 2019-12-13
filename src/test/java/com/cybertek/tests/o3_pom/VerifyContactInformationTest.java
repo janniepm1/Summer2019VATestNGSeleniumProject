bc622a3e913d1a7edf306571c53e56e98053fd83
cinthya velaochaga <janniepm258@gmail.com>
2019-12-12 19:58:41 -0500

    'testVerifyContact'

--- src/test/java/com/cybertek/tests/o3_pom/VerifyContactInformationTest.java
+++ src/test/java/com/cybertek/tests/o3_pom/VerifyContactInformationTest.java
@@ -1,8 +1,12 @@
 package com.cybertek.tests.o3_pom;
 
+import com.cybertek.Utilities.BrowserUtils;
 import com.cybertek.Utilities.ConfigurationReader;
+import com.cybertek.pages.ContactInformationPage;
+import com.cybertek.pages.ContactsPage;
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class VerifyContactInformationTest extends TestBase {
@@ -21,5 +25,31 @@ public class VerifyContactInformationTest extends TestBase {
         extentLogger.info("username: "+username);
         extentLogger.info("password: "+password);
         loginPage.login(username,password);
+
+        //go to costumers then contacts
+        extentLogger.info("Go to costumers -> contacts");
+        ContactsPage contactsPage =new ContactsPage();
+        contactsPage.navigateToModule("Customers","Contacts");
+
+        //click con email
+        extentLogger.info("Click on email mbrackstone9@example.com");
+        //wait before click on email
+        contactsPage.waitUntilLoaderScreenDisappear();
+        contactsPage.getContantEmail("mbrackstone9@example.com").click();
+//verify
+        ContactInformationPage contactInformationPage=new ContactInformationPage();
+        extentLogger.info("Verify that full name is Ms Mariam Brackstone");
+
+        String actualName = contactInformationPage.contactFullname.getText();
+        Assert.assertEquals("Ms Mariam Brackstone",actualName);
+        //verify phoneNumber
+        extentLogger.info("Verify that phone number is 18982323434");
+
+        String actualPhone= contactInformationPage.phone.getText();
+        Assert.assertEquals(actualPhone,"+18982323434");
+
+        String actualEmail= contactInformationPage.email.getText();
+        Assert.assertEquals(actualEmail,"mbrackstone9@example.com");
+
     }
 }
